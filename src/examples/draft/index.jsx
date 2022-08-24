dfd0b4bb6f0887d74d844e9cfe35d37259ce0fa5
yangbiwan <yangbiwan@lasoplanet.com>
2022-08-24 10:46:50 +0800

    draft.js rich styling

--- src/examples/draft/index.jsx
+++ src/examples/draft/index.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { useCallback, useState } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 const DraftEditor = () => {
-  const [editorState, setEditorState] = useState(
-    () => EditorState.createEmpty(),
-  );
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+  const handleBold = useCallback(event => {
+    event.preventDefault()
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'))
+  }, [editorState]);
+
+  const handleCodeBlock = useCallback(event => {
+    event.preventDefault()
+    setEditorState(RichUtils.toggleCode(editorState))
+  }, [editorState]);
 
   return (
     <div className="draft">
+      <button onMouseDown={handleBold}>Bold</button>
+      <button onMouseDown={handleCodeBlock}>Code Block</button>
       <Editor
         editorState={editorState}
         onChange={setEditorState}
